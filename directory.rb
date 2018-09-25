9767b9632d196ecdd51a46188f4998ff2410173b
Tolvic <terence.h.mace@gmail.com>
2018-09-25 10:36:48 +0100

    Add a count to directory

--- directory.rb
+++ directory.rb
@@ -11,3 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+# Print of the total number of students. 
+print "Overall, we have "
+print 11
+puts " great students"
